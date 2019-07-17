f5544700ae98c93ccf926fd64f45f3474ce2de5c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-17 19:28:41 +0200

    test: implement some rules tests

--- firestore/rules-test/src/index.spec.test.ts
+++ firestore/rules-test/src/index.spec.test.ts
@@ -1,16 +1,68 @@
-/* tslint:disable:max-classes-per-file */
-import { FirestoreCollections } from "amerykahospital-personalizedadvice-core";
-import { use as chaiUse } from "chai";
+/* tslint:disable:max-classes-per-file no-console */
+import * as firebase from "@firebase/testing";
+import { FirestoreCollections, Advice } from "amerykahospital-personalizedadvice-core";
+import { expect, use as chaiUse } from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import * as _ from "lodash";
+import * as uuid from "uuid/v4";
+import * as BluebirdPromise from "bluebird";
 import "mocha";
 
 chaiUse(chaiAsPromised);
 
+function mock(o: { auth?: {} }) {
+    const projectId = "unit-testing-" + Date.now();
+
+    const clientAppConfig: any = { projectId };
+    if (o.auth) clientAppConfig.auth = o.auth;
+    const clientApp = firebase.initializeTestApp(clientAppConfig);
+    const clientFirestore = clientApp.firestore();
+
+    const adminApp = firebase.initializeAdminApp({ projectId });
+    const adminFirestore = adminApp.firestore();
+    return { projectId, clientApp, adminApp, clientFirestore, adminFirestore };
+}
+
+function sampleAdvice(uid?: string) {
+    const advice: Advice = {
+        id: uuid(),
+        patientName: "patient-" + uuid(),
+        medicalprofessionalName: "medicalprofessional-" + uuid(),
+        parentPhoneNumber: "123123123",
+        dateISO: new Date().toISOString(),
+        advice: "advice-" + uuid(),
+    };
+    if (uid) advice.uid = uid; // firebase catches the difference between undefined and nonexistent attr
+    return advice;
+}
+
+afterEach(async () => {
+    try {
+        await Promise.all(firebase.apps().map(app => app.delete()));
+    } catch (error) {
+        console.warn("Warning: Error in firebase shutdown " + error);
+    }
+});
+
 describe("Firebase rules", () => {
     describe("Collection " + FirestoreCollections.ADVICES_COLLECTION_KEY, () => {
+        const collName = FirestoreCollections.ADVICES_COLLECTION_KEY;
         describe("get", () => {
-            it.skip("Is not allowed when user is not authenticated");
+            it("Is not allowed when user is not authenticated", async () => {
+                const { adminFirestore, clientFirestore } = mock({ auth: undefined });
+                await adminFirestore
+                    .collection(collName)
+                    .doc("doc")
+                    .set(sampleAdvice());
+
+                await expect(
+                    clientFirestore
+                        .collection(collName)
+                        .doc("doc")
+                        .get(),
+                ).to.eventually.be.rejectedWith("FirebaseError");
+            });
+
             it.skip("Is allowed when advice belongs to user");
             it.skip("Is allowed when advice belongs to different user");
             it.skip("Is allowed when advice does not belong to a user and authenticated");
@@ -22,7 +74,17 @@ describe("Firebase rules", () => {
             it.skip("Lists only advices that belongs to a specific user");
             it.skip("Medical professional can list all advices");
             it.skip("Authenticated user can not list pending advices");
-            it.skip("Non authenticated user can not list anything");
+            it("Non authenticated user can not list anything", async () => {
+                const { adminFirestore, clientFirestore } = mock({ auth: undefined });
+                for (let i = 0; i < 5; i++) {
+                    await adminFirestore
+                        .collection(collName)
+                        .doc(`doc${i}`)
+                        .set(sampleAdvice());
+                }
+
+                await expect(clientFirestore.collection(collName).get()).to.eventually.be.rejectedWith("FirebaseError");
+            });
         });
 
         describe("create", () => {
