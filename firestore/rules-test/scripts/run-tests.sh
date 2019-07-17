c8266ac6604861313ec982b05e660a345c326334
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-17 20:50:50 +0200

    ci: fix test runner

---
+++ firestore/rules-test/scripts/run-tests.sh
@@ -0,0 +1,8 @@
+    
+#!/usr/bin/env bash
+set -e # fail on first error
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/."
+cd "${DIR}"
+
+firebase setup:emulators:firestore
+firebase emulators:exec --token="$FIREBASE_EMULATOR_TOKEN" --only firestore "./_exec-tests.sh"
