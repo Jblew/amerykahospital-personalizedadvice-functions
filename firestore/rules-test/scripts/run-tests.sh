7ce9e84efadc60b1c1cd23b15bd24dd9e93e00db
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-17 18:35:19 +0200

    ci: change emulator runner

--- firestore/rules-test/scripts/run-tests.sh
+++
@@ -1,8 +0,0 @@
-    
-#!/usr/bin/env bash
-set -e # fail on first error
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/."
-cd "${DIR}"
-
-firebase setup:emulators:firestore
-firebase emulators:exec --only firestore "./_exec-tests.sh"
