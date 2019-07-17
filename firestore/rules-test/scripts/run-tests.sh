278f666740a02faeec214a71f30404118bed1e8e
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-17 18:32:04 +0200

    ci: setup firestore emulator

--- firestore/rules-test/scripts/run-tests.sh
+++ firestore/rules-test/scripts/run-tests.sh
@@ -4,4 +4,5 @@ set -e # fail on first error
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/."
 cd "${DIR}"
 
+firebase setup:emulators:firestore
 firebase emulators:exec --only firestore "./_exec-tests.sh"
