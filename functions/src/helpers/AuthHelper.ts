3eb4ad2bd9e1588b23ed1f2eb8a8bdc5d76ff916
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-23 20:16:59 +0200

    feat: implement AuthHelper

--- functions/src/helpers/AuthHelper.ts
+++ functions/src/helpers/AuthHelper.ts
@@ -1,7 +1,36 @@
-import { FirestoreCollections } from "amerykahospital-personalizedadvices-core";
+import { FirestoreCollections } from "amerykahospital-personalizedadvice-core";
+import * as functions from "firebase-functions";
 
 export class AuthHelper {
-    public static async isAuthenticatedMedicalProfessional(uid: string, db: firebase.firestore.Firestore) {
+    public static async assertAuthenticated(context: functions.https.CallableContext) {
+        if (!AuthHelper.isAuthenticated(context)) {
+            throw new functions.https.HttpsError("permission-denied", "Please authenticate");
+        }
+    }
+
+    public static async assertUserIsMedicalProfessional(
+        context: functions.https.CallableContext,
+        db: FirebaseFirestore.Firestore,
+    ) {
+        if (
+            !context.auth ||
+            !context.auth.uid ||
+            !(await AuthHelper.isAuthenticatedMedicalProfessional(context.auth.uid, db))
+        ) {
+            throw new functions.https.HttpsError(
+                "permission-denied",
+                "You must be a medical professional to access this function. Please contact system administrator",
+            );
+        }
+    }
+
+    private static isAuthenticated(
+        context: functions.https.CallableContext,
+    ): context is functions.https.CallableContext & { auth: { uid: string } } {
+        return !!context.auth && !!context.auth.uid;
+    }
+
+    private static async isAuthenticatedMedicalProfessional(uid: string, db: FirebaseFirestore.Firestore) {
         const medicalprofessionalDoc = await db
             .collection(FirestoreCollections.MEDICALPROFESSIONAL_UIDS_COLLECTION)
             .doc(uid)
