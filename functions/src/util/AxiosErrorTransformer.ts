4cc4f221eca3f4a02c9c2bcfe4bdcca0b5da2c56
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-25 00:03:56 +0200

    fix(functions): log prettier error messages in add advice function

--- functions/src/util/AxiosErrorTransformer.ts
+++ functions/src/util/AxiosErrorTransformer.ts
@@ -6,6 +6,11 @@ export class AxiosErrorTransformer {
             return await fn();
         } catch (error) {
             if (error.response) {
+                const resp = error.response;
+                if (resp.data && resp.data.error) {
+                    const errData = resp.data.error;
+                    throw new Error(errData.message || JSON.stringify(errData));
+                }
                 const msg =
                     "Response error: " +
                     error.name +
@@ -14,7 +19,6 @@ export class AxiosErrorTransformer {
                     ", error response: " +
                     JSON.stringify(error.response.data);
                 const errObject: any = new Error(msg);
-                errObject.vault = true;
                 errObject.response = { status: _.get(error, "response.status", 0) };
 
                 throw errObject;
