96a56a9dccdcb4c7f410bab757e0ba835f0ba8e5
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-25 00:40:24 +0200

    feat(functions): log sent messages

--- functions/src/functions/addadvice/AdviceDeepLinkGenerator.ts
+++ functions/src/functions/addadvice/AdviceDeepLinkGenerator.ts
@@ -2,6 +2,7 @@ import { Advice, FIREBASE_CONFIG } from "amerykahospital-personalizedadvice-core
 
 import { DynamicLinksAdapter } from "../../adapters/DynamicLinksAdapter";
 import { i18n_pl } from "../../i18n_pl";
+import { Log } from "../../Log";
 
 export class AdviceDeepLinkGenerator {
     public static async obtainDeepLink(adviceId: string): Promise<string> {
@@ -15,6 +16,7 @@ export class AdviceDeepLinkGenerator {
         const msg = i18n_pl.adviceSMSText
             .replace("$medicalProfessionalName", advice.medicalprofessionalName)
             .replace("$link", link);
+        Log.log().info("Sending message " + msg);
         return msg;
     }
 }
