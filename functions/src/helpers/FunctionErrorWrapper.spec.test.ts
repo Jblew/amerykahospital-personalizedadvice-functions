27d66a3506665876fdb6c8dbcb948cf570411762
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-24 19:32:07 +0200

    test(functions): test FunctionsErrorWrapper

---
+++ functions/src/helpers/FunctionErrorWrapper.spec.test.ts
@@ -0,0 +1,39 @@
+/* tslint:disable:max-classes-per-file no-console */
+import { expect, use as chaiUse } from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+import * as _ from "lodash";
+import "mocha";
+import * as functions from "firebase-functions";
+
+import { FunctionErrorWrapper } from "./FunctionErrorWrapper";
+
+chaiUse(chaiAsPromised);
+
+describe("FunctionErrorWrapper", () => {
+    describe("wrap", () => {
+        it("Wraps standard JS errors inside HttpsError", async () => {
+            async function throwingFn() {
+                const err = new Error("Some error");
+                (err as any).identifyingProp = "v";
+                throw err;
+            }
+
+            await expect(FunctionErrorWrapper.wrap(async () => throwingFn()))
+                .to.eventually.be.rejectedWith("Error")
+                .that.haveOwnProperty("code");
+        });
+
+        it("Rethrows HttpsError errors", async () => {
+            async function throwingFn() {
+                const err = new functions.https.HttpsError("unknown", "SomeHttpsError");
+
+                (err as any).identifyingProp = "v";
+                throw err;
+            }
+
+            await expect(FunctionErrorWrapper.wrap(async () => throwingFn()))
+                .to.eventually.be.rejectedWith("SomeHttpsError")
+                .that.haveOwnProperty("identifyingProp");
+        });
+    });
+});
