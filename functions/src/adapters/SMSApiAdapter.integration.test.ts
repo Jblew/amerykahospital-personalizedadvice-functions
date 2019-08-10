1ac3092a7fd52bd7009099ed99c715f0e65c4839
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-10 14:19:22 +0200

    test: temporarily skip SMSApiAdapter tests

--- functions/src/adapters/SMSApiAdapter.integration.test.ts
+++ functions/src/adapters/SMSApiAdapter.integration.test.ts
@@ -8,15 +8,14 @@ import * as uuid from "uuid/v4";
 import { SMSApiAdapter } from "./SMSApiAdapter";
 
 chaiUse(chaiAsPromised);
+describe.skip("SMSApiAdapter", () => {
+    function mock() {
+        const adapter = new SMSApiAdapter({ test: true, from: "Test" });
+        const phoneNumber = "508173995"; // (Math.floor(Math.random() * 1000000000) + "").padStart(9, "0");
+        const message = `msg ${uuid()}`;
+        return { adapter, phoneNumber, message };
+    }
 
-function mock() {
-    const adapter = new SMSApiAdapter({ test: true, from: "Test" });
-    const phoneNumber = "508173995"; // (Math.floor(Math.random() * 1000000000) + "").padStart(9, "0");
-    const message = `msg ${uuid()}`;
-    return { adapter, phoneNumber, message };
-}
-
-describe("SMSApiAdapter", () => {
     it("Sends message without error", async () => {
         const { adapter, phoneNumber, message } = mock();
 
