169fcd904b058d15eebc9f332ef06c9db176ad9d
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-12 21:25:17 +0200

    test: test functions resolver

---
+++ functions/src/index_resolver.integration.test.ts
@@ -0,0 +1,28 @@
+/* tslint:disable no-unused-expression no-console */
+import { IntegrationTestsEnvironment } from "./_test/IntegrationTestsEnvironment";
+import { expect } from "./_test/test_environment";
+import { resolveAddAdvice, resolveImportAdviceToUser, resolveSendSMS } from "./index_resolver";
+
+describe("index_resolver", function() {
+    this.timeout(4000);
+
+    const env = new IntegrationTestsEnvironment();
+
+    beforeEach(async () => await env.prepareEach());
+    afterEach(async () => await env.cleanupEach());
+
+    it("AddAdvice is resolved properly", () => {
+        const resolvedFunction = resolveAddAdvice(env.getContainer());
+        expect(resolvedFunction).to.not.be.undefined;
+    });
+
+    it("SendSMS is resolved properly", () => {
+        const resolvedFunction = resolveSendSMS(env.getContainer());
+        expect(resolvedFunction).to.not.be.undefined;
+    });
+
+    it("ImportAdviceToUser is resolved properly", () => {
+        const resolvedFunction = resolveImportAdviceToUser(env.getContainer());
+        expect(resolvedFunction).to.not.be.undefined;
+    });
+});
