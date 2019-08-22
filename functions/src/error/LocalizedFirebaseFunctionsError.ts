ad4446ee8250c1d3c58a2d92243128684c1f4f0e
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-22 22:21:54 +0200

    refactor: move transferable error logic to localized-error package

--- functions/src/error/LocalizedFirebaseFunctionsError.ts
+++ functions/src/error/LocalizedFirebaseFunctionsError.ts
@@ -1,13 +1,10 @@
 import * as functions from "firebase-functions";
-import { LocalizedError, LocalizedErrorFactory } from "localized-error";
+import { TransferableLocalizedError } from "localized-error";
 
-export type LocalizedFirebaseFunctionsError<TYPE extends string> = Error &
-    LocalizedError & {
-        details: {
-            type: TYPE;
-            advanced: string;
-        };
-    };
+export type LocalizedFirebaseFunctionsError<TYPE extends string> = TransferableLocalizedError<
+    TYPE,
+    functions.https.FunctionsErrorCode
+>;
 
 export namespace LocalizedFirebaseFunctionsError {
     export function make<TYPE extends string>(params: {
@@ -16,13 +13,6 @@ export namespace LocalizedFirebaseFunctionsError {
         advanced: string;
         localizedMessage: { EN: string; [x: string]: string };
     }): LocalizedFirebaseFunctionsError<TYPE> {
-        const unknownFirebaseError = new functions.https.HttpsError(params.code, params.localizedMessage.EN, {
-            type: params.type,
-            advanced: params.advanced,
-        });
-        return LocalizedErrorFactory.make(
-            unknownFirebaseError,
-            params.localizedMessage,
-        ) as LocalizedFirebaseFunctionsError<TYPE>;
+        return TransferableLocalizedError.make(functions.https.HttpsError, params);
     }
 }
