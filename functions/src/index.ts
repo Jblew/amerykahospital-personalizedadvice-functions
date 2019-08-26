438dab8bf09d168ff69c505e32bf0b62e3ef07ba
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-26 14:33:33 +0200

    fix: remove rate limiter test functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,38 +1,17 @@
 // tslint:disable no-console no-string-literal
 import { FirebaseFunctionDefinitions } from "amerykahospital-personalizedadvice-core";
 import * as functions from "firebase-functions";
-import FirebaseFunctionsRateLimiter from "firebase-functions-rate-limiter";
 import "reflect-metadata";
 
 import { resolveAddAdvice, resolveImportAdviceToUser, resolveSendSMS } from "./index_resolver";
 import containerFactory from "./inversify.config";
-import TYPES from "./TYPES";
 
 const container = containerFactory();
 exports[FirebaseFunctionDefinitions.AddAdvice.NAME] = resolveAddAdvice(container);
 exports[FirebaseFunctionDefinitions.SendSMS.NAME] = resolveSendSMS(container);
 exports[FirebaseFunctionDefinitions.ImportAdviceToUser.NAME] = resolveImportAdviceToUser(container);
 
-const rlimiterRealtimeDB = FirebaseFunctionsRateLimiter.withRealtimeDbBackend(
-    {
-        name: "rlimiterRealtimeDB",
-        maxCalls: 2,
-        periodSeconds: 30,
-    },
-    container.get(TYPES.RealtimeDatabase),
-);
-exports["test_rate_limiter_realtimedb"] = functions.https.onRequest(async (req, res) => {
-    await rlimiterRealtimeDB.rejectOnQuotaExceeded();
-    res.send("OK");
-});
 
-const rlimiterFirestore = FirebaseFunctionsRateLimiter.withFirestoreBackend(
-    {
-        name: "rlimiterFirestore",
-        maxCalls: 2,
-        periodSeconds: 30,
-    },
-    container.get(TYPES.Firestore),
 );
 exports["test_rate_limiter_firestore"] = functions.https.onRequest(async (req, res) => {
     await rlimiterFirestore.rejectOnQuotaExceeded();
