0b2b5a77b6e4b94f629ecc385ba7a581c39f3e60
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-26 14:32:39 +0200

    refactor: extract UpstreamHandler from AuthenticatedFunctionHandler

--- functions/src/functions/handlers/AuthenticatedFunctionHandler.ts
+++ functions/src/functions/handlers/AuthenticatedFunctionHandler.ts
@@ -1,13 +1,10 @@
-import { Handler } from "amerykahospital-personalizedadvice-businesslogic";
 import * as functions from "firebase-functions";
 import FirebaseFunctionsRateLimiter from "firebase-functions-rate-limiter";
 
 import { PerUserLimitExceededError } from "../../error/PerUserLimitExceededError";
 import { AuthHelper } from "../../helpers/auth/AuthHelper";
 
-type UpstreamHandler<INPUT_TYPE, RESULT_TYPE> = Handler<
-    (data: INPUT_TYPE, context: functions.https.CallableContext) => Promise<RESULT_TYPE>
->;
+import { UpstreamHandler } from "./UpstreamHandler";
 
 export class AuthenticatedFunctionHandler<INPUT_TYPE, RESULT_TYPE> implements UpstreamHandler<INPUT_TYPE, RESULT_TYPE> {
     private authHelper: AuthHelper;
