af300161096d079e5c4799f20db03acc67010f3e
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-01-25 21:43:50 +0100

    fix: initialize admin app with default credentials

--- functions/src/providers/FirebaseAppFactory.ts
+++ functions/src/providers/FirebaseAppFactory.ts
@@ -1,4 +1,3 @@
 import * as admin from "firebase-admin";
-import * as functions from "firebase-functions";
 import * as inversify from "inversify";
-export default (context: inversify.interfaces.Context) => admin.initializeApp(functions.config().firebase);
+export default (context: inversify.interfaces.Context) => admin.initializeApp();
