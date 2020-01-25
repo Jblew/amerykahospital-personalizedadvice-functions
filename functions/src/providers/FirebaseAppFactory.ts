eb39ffd4108ef89155db946eef3e44fd500f09df
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-01-25 22:07:29 +0100

    feat: debug firebase app

--- functions/src/providers/FirebaseAppFactory.ts
+++ functions/src/providers/FirebaseAppFactory.ts
@@ -1,6 +1,8 @@
+// tslint:disable no-console
 import * as admin from "firebase-admin";
 import * as inversify from "inversify";
 
 import { FIREBASE_CONFIG } from "../settings";
 
+console.log("Initialized admin app with credentials", FIREBASE_CONFIG);
 export default (context: inversify.interfaces.Context) => admin.initializeApp(FIREBASE_CONFIG);
