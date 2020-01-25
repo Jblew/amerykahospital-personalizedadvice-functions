88406b3404c71909e9cf919008c5d4bb081c98bf
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-01-25 17:42:55 +0100

    fix: access gcp secret by path

--- functions/src/adapters/obtainSMSApiToken.ts
+++ functions/src/adapters/obtainSMSApiToken.ts
@@ -1,9 +1,14 @@
 import {SecretManagerServiceClient} from "@google-cloud/secret-manager";
 const client = new SecretManagerServiceClient();
+import { FIREBASE_CONFIG } from "../settings";
+
+const projectName = FIREBASE_CONFIG.projectId;
+const secretName = "smsapi-token";
+const secretPath = `projects/${projectName}/secrets/${secretName}/versions/latest`;
 
 export async function obtainSMSApiToken(): Promise<string> {
     try {
-        const [accessResponse] =  await client.accessSecretVersion({ name: "smsapi-token" });
+        const [accessResponse] =  await client.accessSecretVersion({ name: secretPath });
         if (!accessResponse.payload) throw new Error("Payload is null");
         return accessResponse.payload.data!.toString();
     } catch (error) {
